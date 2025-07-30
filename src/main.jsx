8a09cb27418604e9303e027744d5d596c251d8bc
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-07-30 08:33:50 +0700

    feat: Add signin, signup, and moviedesktop imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import MovieDekstop from "./pages/MovieDekstop.jsx";
+import SignIn from "./pages/auth/SignIn.jsx";
+import SignUp from "./pages/auth/SingUp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <SignUp /> */}
+    {/* <SignIn /> */}
+    <MovieDekstop />
   </StrictMode>
 );
